b8b47e32c9e45ea7725ecf0f50b642ff4313997d
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-09-16 13:30:04 +0700

    productType Create

--- app/pages/productType.vue
+++ app/pages/productType.vue
@@ -1,5 +1,8 @@
 <script setup>
 import Modal from '~/components/modal.vue'
+import config from '~/config'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const showModal = ref(false)
 
@@ -8,8 +11,27 @@ definePageMeta({
     layout: 'admin'
 })
 
+const name = ref('')
+const remark = ref('')
+
 const closeModal = () =>{
     showModal.value = false
+}
+
+const save = async ()=>{
+    try {
+        const payload = {
+            name: name.value,
+            remark: remark.value
+        }
+        const res = await axios.post(`${config.apiServer}/api/productType/create`, payload)
+    } catch (e) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: e.message,
+        })
+    }
 }
 </script>
 
@@ -24,11 +46,11 @@ const closeModal = () =>{
 
     <Modal v-if="showModal" title="เพิ่มประเภทสินค้า" @close="closeModal">
         <div>ชื่อ</div>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="name">
 
         <div class="mt-3">หมายเหตุ</div>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="remark">
 
-        <button class="btn btn-primary mt-3"><i class="fa fa-check me-2"></i>บันทึก</button>
+        <button class="btn btn-primary mt-3" @click="save"><i class="fa fa-check me-2" ></i>บันทึก</button>
     </Modal>
 </template>
